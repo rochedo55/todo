74af23eb303b9f41d23182ba68693abba56167c1
Nalbert Gabriel Melo Leal <nalbertg@outlook.com>
2020-03-11 12:34:49 -0300

    react router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+// import logo from './logo.svg';
 import './App.css';
+
+import Home from './components/pages/Home.js';
+import About from './components/pages/About.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/">
+          <Home />
+        </Route>
+        <Route path="/ok">
+          <About />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/components/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Home extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    render() {
+        return <div>hello</div>
+    }
+}
+
+export default Home;
